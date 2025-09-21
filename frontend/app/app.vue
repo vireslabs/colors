1eeeb88d3c66b46571d7cd4169d7264d63b20b26
roma_marshall <roma_marshall@outlook.com>
2025-09-21 23:06:31 +0200

    add mintPrice ether

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -47,7 +47,9 @@ async function mintNFT() {
     const signer = await provider.getSigner();
     const contract = new ethers.Contract(contractAddress, abi, signer);
     status.value = "Sending transaction...";
-    const tx = await contract.mint(color.value);
+    const tx = await contract.mint(color.value, {
+      value: parseEther("0.001"), // mintPrice = 0.001 ether
+    });
     await tx.wait();
     status.value = `NFT minted! Address: ${address.value}`;
   } catch (err) {
